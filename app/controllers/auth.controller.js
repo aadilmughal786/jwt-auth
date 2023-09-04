4dd291619fa510fc2259ed865645ed8edc8feb85
Aadil Mugal <aadilshe786@gmail.com>
2023-09-04 23:25:23 +0530

    feat : Add controler

---
+++ app/controllers/auth.controller.js
@@ -0,0 +1,92 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+const config = require("../config/auth.config");
+const db = require("../models");
+const User = db.user;
+const Role = db.role;
+
+exports.signup = async (req, res) => {
+  try {
+    const { username, email, password, roles } = req.body;
+
+    // Check if the username or email already exists
+    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ message: "Username or email is already in use." });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 8);
+
+    // Determine roles
+    let userRoles = [];
+    if (roles && roles.length > 0) {
+      const foundRoles = await Role.find({ name: { $in: roles } });
+      userRoles = foundRoles.map((role) => role._id);
+    } else {
+      const defaultRole = await Role.findOne({ name: "user" });
+      userRoles = [defaultRole._id];
+    }
+
+    // Create a new user
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+      roles: userRoles,
+    });
+
+    // Save the user to the database
+    await newUser.save();
+
+    res.status(201).json({ message: "User was registered successfully!" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.signin = async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username }).populate(
+      "roles",
+      "-__v"
+    );
+
+    if (!user) {
+      return res.status(404).json({ message: "User Not found." });
+    }
+
+    const passwordIsValid = bcrypt.compareSync(
+      req.body.password,
+      user.password
+    );
+
+    if (!passwordIsValid) {
+      return res
+        .status(401)
+        .json({ accessToken: null, message: "Invalid Password!" });
+    }
+
+    const token = jwt.sign({ id: user.id }, config.secret, {
+      algorithm: "HS256",
+      expiresIn: 24 * 60 * 60, // 24 hours
+    });
+
+    const authorities = user.roles.map(
+      (role) => "ROLE_" + role.name.toUpperCase()
+    );
+
+    return res.status(200).json({
+      id: user._id,
+      username: user.username,
+      email: user.email,
+      roles: authorities,
+      accessToken: token,
+    });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
--- app/middlewares/verify-sign-up.js
+++ app/middlewares/verify-sign-up.js
@@ -2,57 +2,50 @@ const db = require("../models");
 const ROLES = db.ROLES;
 const User = db.user;
 
-const checkDuplicateUsernameOrEmail = (req, res, next) => {
-  // Username
-  User.findOne({
-    username: req.body.username,
-  }).exec((err, user) => {
-    if (err) {
-      res.status(500).send({ message: err });
-      return;
-    }
-
+const checkDuplicateUsername = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
     if (user) {
-      res.status(400).send({ message: "Failed! Username is already in use!" });
-      return;
+      return res
+        .status(400)
+        .json({ message: "Failed! Username is already in use!" });
     }
+    next();
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
-    // Email
-    User.findOne({
-      email: req.body.email,
-    }).exec((err, user) => {
-      if (err) {
-        res.status(500).send({ message: err });
-        return;
-      }
-
-      if (user) {
-        res.status(400).send({ message: "Failed! Email is already in use!" });
-        return;
-      }
-
-      next();
-    });
-  });
+const checkDuplicateEmail = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res
+        .status(400)
+        .json({ message: "Failed! Email is already in use!" });
+    }
+    next();
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 const checkRolesExisted = (req, res, next) => {
   if (req.body.roles) {
     for (let i = 0; i < req.body.roles.length; i++) {
       if (!ROLES.includes(req.body.roles[i])) {
-        res.status(400).send({
+        return res.status(400).json({
           message: `Failed! Role ${req.body.roles[i]} does not exist!`,
         });
-        return;
       }
     }
   }
-
   next();
 };
 
 const verifySignUp = {
-  checkDuplicateUsernameOrEmail,
+  checkDuplicateUsername,
+  checkDuplicateEmail,
   checkRolesExisted,
 };
 
