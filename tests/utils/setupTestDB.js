5bf710e4fd2b091606d6641dc710bd8d64b9cb4f
corners2wall <akosto4kin@gmail.com>
2023-09-12 16:55:27 +0300

    fix: delete commended code

--- tests/utils/setupTestDB.js
+++ tests/utils/setupTestDB.js
@@ -6,10 +6,6 @@ const setupTestDB = () => {
     await mongoose.connect(config.URL);
   });
 
-  // beforeEach(async () => {
-  //   await Promise.all(Object.values(mongoose.connection.collections).map(async (collection) => collection.deleteMany()));
-  // });
-
   afterAll(async () => {
     await mongoose.disconnect();
   });
