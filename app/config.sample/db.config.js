20c181d14bfd21b965bbe2bda3c697e826fc90d7
corners2wall <akosto4kin@gmail.com>
2023-09-14 17:33:21 +0300

    feat: revert db config

--- app/config.sample/db.config.js
+++ app/config.sample/db.config.js
@@ -1,3 +1,9 @@
+const dbConfig = {
+  USER: 'user-name',
+  PASSWORD: 'password',
+  DB: 'db-name',
+};
+
 module.exports = {
-  URL: 'mongodb://localhost:27017',
+  URL: `mongodb+srv://${dbConfig.USER}:${dbConfig.PASSWORD}@cluster123.xyz.mongodb.net/${dbConfig.DB}`,
 };
